69d17360915ec94cf0cc6f811938fe6d78d4986f
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-08-29 07:58:11 +0000

    admin-ui -- tag 1.4.0 -- Nouvelle version compatible v1.4 + v2.0

---
+++ admin-ui/tags/1.4.0/Items/Emoji.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Fonctionnalités :
+ * - Désactivation de la prise en charge des emoji.
+ */
+
+namespace tiFy\Plugins\AdminUi\Items;
+
+use tiFy\App\Dependency\AbstractAppDependency;
+
+class Emoji extends AbstractAppDependency
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        if (config('admin-ui.disable_emoji')) :
+            $this->app->appAddAction('init', [$this, 'init']);
+            $this->app->appAddFilter('tiny_mce_plugins', [$this, 'tiny_mce_plugins']);
+            $this->app->appAddFilter('wp_resource_hints', [$this, 'wp_resource_hints'], 10, 2);
+        endif;
+    }
+
+    /**
+     * Initialisation globale de Wordpress.
+     *
+     * @return null
+     */
+    public function init()
+    {
+        \remove_action('wp_head', 'print_emoji_detection_script', 7);
+        \remove_action('admin_print_scripts', 'print_emoji_detection_script');
+        \remove_action('wp_print_styles', 'print_emoji_styles');
+        \remove_action('admin_print_styles', 'print_emoji_styles');
+        \remove_filter('the_content_feed', 'wp_staticize_emoji');
+        \remove_filter('comment_text_rss', 'wp_staticize_emoji');
+        \remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    }
+
+    /**
+     * Filtrage de la liste des plugins tinyMCE.
+     *
+     * @param array $plugins Liste des plugins tinyMCE actifs.
+     *
+     * @return array
+     */
+    public function tiny_mce_plugins($plugins)
+    {
+        if (is_array($plugins)) :
+            return array_diff($plugins, ['wpemoji']);
+        else :
+            return [];
+        endif;
+    }
+
+    /**
+     * Récupération de ressources pré-affichées dans le navigateur.
+     *
+     * @param array $urls Liste des urls des ressources pré-affichées.
+     * @param string $relation_type Type de relation des urls à pré-afficher.
+     *
+     * @return array
+     */
+    public function wp_resource_hints($urls, $relation_type)
+    {
+        if ('dns-prefetch' == $relation_type) :
+            /** This filter is documented in wp-includes/formatting.php */
+            $emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2.4/svg/');
+
+            $urls = array_diff($urls, [$emoji_svg_url]);
+        endif;
+
+        return $urls;
+    }
+}
