c5dce347737b7e634bb3d5fdb06d3f48286f2681
Jordy Manner <jordy.manner@milkcreation.fr>
2018-12-31 18:20:25 +0000

    admin-ui -- branch 2.0 -- Correction composer.json

--- AdminUi.php
+++ AdminUi.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/admin-ui
  * @namespace \tiFy\Plugins\AdminUi
- * @version 2.0.5
+ * @version 2.0.6
  */
 
 namespace tiFy\Plugins\AdminUi;
